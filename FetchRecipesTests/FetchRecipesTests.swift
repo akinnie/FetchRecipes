f891532275d97e8835ac20e4b2be5d5d40f3e69e
akinnie <akinnie@mac.com>
2025-02-10 15:12:52 +1100

    Initial Commit

---
+++ FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Andrew Kinnie on 2/10/25.
+//
+
+import Testing
+@testable import FetchRecipes
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
